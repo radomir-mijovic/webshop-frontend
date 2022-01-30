289c31214fe48fd28215e1a2b91a07fc9904e9b4
Radomir Mijovic <batomijovic4@gmail.com>
2022-01-30 13:06:23 +0100

    feat: added styled component and framer motion package

---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {NavbarStyled} from "./NavbarStyled";
+
+const Navbar = () => {
+    return (
+        <NavbarStyled>
+
+        </NavbarStyled>
+    );
+};
+
+export default Navbar;
