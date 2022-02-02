a9c7e4a2cdfa728ca71da29030453cb0044cf8bb
Radomir Mijovic <batomijovic4@gmail.com>
2022-02-02 22:11:27 +0100

    fix: fixed initial style flickering

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,49 @@
-import {Main, Head, Html, NextScript} from "next/document";
+import Document, {Main, Head, Html, NextScript} from "next/document";
+import {ServerStyleSheet} from 'styled-components'
+import React from "react";
 
-export default function Document() {
-    return (
-        <Html>
-            <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
-                <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500;700&display=swap"
-                      rel="stylesheet"/>
-                <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap"
-                      rel="stylesheet"/>
-            </Head>
-            <body>
-            <Main/>
-            <NextScript/>
-            </body>
-        </Html>
-    )
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+                    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500;700&display=swap"
+                          rel="stylesheet"/>
+                    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap"
+                          rel="stylesheet"/>
+                </Head>
+                <body>
+                <Main/>
+                <NextScript/>
+                </body>
+            </Html>
+        )
+    }
 }
+
+MyDocument.getInitialProps = async (ctx) => {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    ctx.renderPage = () =>
+        originalRenderPage({
+            enhanceApp: (App) => (props) =>
+                sheet.collectStyles(<App {...props} />),
+        })
+
+    const initialProps = await Document.getInitialProps(ctx)
+    return {
+        ...initialProps,
+        styles: (
+            <>
+                {initialProps.styles}
+                {sheet.getStyleElement()}
+            </>
+        ),
+    }
+
+}
+
+export default MyDocument;
